2356c62143e7370077e18258fdeccc54d70b8b92
UnaJeon <unajeon@gmail.com>
2020-05-07 22:14:23 +0000

    Added a GradeForm Component

---
+++ components/grade-form.js
@@ -0,0 +1,13 @@
+class GradeForm{
+  constructor(formElement){
+    this.formElement = formElement;
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  onSubmit(createGrade){
+    this.createGrade;
+  }
+  handleSubmit(event){
+    preventDefault();
+    console.log('hi')
+  }
+  }
