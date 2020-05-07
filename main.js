125f6cd4b44039bb337a4f794b5330aa7bec1f2f
UnaJeon <unajeon@gmail.com>
2020-05-07 04:53:08 +0000

    Modified HTML & added a PageHeader component

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-
+var header = document.querySelectorAll("header");
+var pageHeader = new PageHeader(header);
 var table = document.querySelectorAll(".table")
 var gradetable =new GradeTable(table)
-var app = new App(gradetable);
+var app = new App(gradetable,pageHeader);
 
 app.start();
